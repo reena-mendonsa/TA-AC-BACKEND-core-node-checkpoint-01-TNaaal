c730a131b69e439cb098a7145c28ddf148ab9f20
Reena Mendonsa <reenamendonsa@gmail.com>
2021-11-27 15:45:45 +0530

    modifications

--- block-BNaaep/server.js
+++ block-BNaaep/server.js
@@ -13,12 +13,14 @@ function handleRequest(req,res){
 
       store +=chunk;
   });
-  if(req.method==='GET'&& req.url==='/'){
+
+  req.on('end', () => {
+    if(req.method==='GET'&& req.url==='/'){
       fs.readFile('./index.html',(err,content)=>{
           res.setHeader('Content-Type','text/html');
           res.write(content);
           res.end();
-      });
+     });
   }
   else if (req.method==='GET' && req.url ==='/about'){
       fs.readFile('./about.html',(err,content)=>{
@@ -45,64 +47,102 @@ function handleRequest(req,res){
       res.end(content);
     });
   } else if (req.method === 'GET' && req.url === '/contact') {
-    fs.readFile('./contactform.html', (err, content) => {
+    fs.readFile('./contact.html', (err, content) => {
       res.setHeader('Content-Type', 'text/html');
       res.end(content);
     });
   }
-  req.on('end', () => {
-    if (req.method === 'POST' && req.url === '/form') {
-      var username = JSON.parse(store).username;
-      console.log(username);
-      let parseData = qs.parse(store);
-      fs.open(
-        __dirname + '/contacts/' + username + '.json',
-        'wx',
-        (err, fd) => {
-          if (err) return res.end('username already taken');
-          fs.writeFile(fd,store , (err) => {
-            if (err) return console.log(err);
-            fs.close(fd, () => {
-              return res.end(`${username} contacts saved`);
+ 
+    else if (req.method === 'POST' && req.url === '/form') {
+      let formatData = req.headers['content-type'];
+      if(formatData ==='application/json'){
+        var username = JSON.parse(store).username;
+       
+        fs.open(
+          __dirname + '/contacts/' + username + '.json',
+          'wx',
+          (err, fd) => {
+            if (err) return res.end('username already taken');
+            fs.writeFile(fd,store , (err) => {
+              if (err) return console.log(err);
+              fs.close(fd, () => {
+                return res.end(`${username} contacts saved`);
+              });
             });
-          });
+          }
+        );
+      }else{
+        var username =qs.parse(store).username ;
+        if(username === ''){
+          return res.end('Username is mandatory!');
         }
-      );
+        let parseData = qs.parse(store);
+        console.log(parseData);
+        fs.open(
+          __dirname + '/contacts/' + username + '.json',
+          'wx',
+          (err, fd) => {
+            if (err) return res.end('username already taken');
+            fs.writeFile(fd,JSON.stringify(parseData), (err) => {
+              if (err) return console.log(err);
+              fs.close(fd, () => {
+                return res.end(`${username} contacts saved`);
+              });
+            });
+          }
+        );
+      }
+      
+      
+      
+      
     }
-    if (req.method === 'GET' && parsedUrl.pathname === '/users') {
-      var username = parsedUrl.query.username;
-      console.log(parsedUrl);
-      fs.readFile(
-        __dirname + '/contacts/' + username + '.json',
-        (err, content) => {
-          res.setHeader('Content-Type', 'application/json');
-          let parseData = JSON.parse(content);
+    else if (req.method === 'GET' && parsedUrl.pathname === '/users') {
+      
+      console.log(Object.keys(parsedUrl.query).length);
+      if(Object.keys(parsedUrl.query).length !== 0){
+        var username = parsedUrl.query.username;
+        fs.readFile(
+          __dirname + '/contacts/' + username + '.json',
+          (err, content) => {
+            res.setHeader('Content-Type', 'text/html');
+            let parseData = JSON.parse(content);
 
-          res.end(`<h1>${parseData.name}</h1>
-                <h2>${parseData.email}</h2>
-                <h2>${parseData.username}</h2>
-                <h2>${parseData.age}</h2>
-                <h2>${parseData.bio}</h2>`);
-        }
-      );
-    }
-    
-    if (req.method === 'GET' && parsedUrl.pathname === '/allusers') {
+             res.end(`<h1>Name:${parseData.name}</h1>
+                <h2>Email:${parseData.email}</h2>
+                <h2>Username:${parseData.username}</h2>
+                <h2>Age:${parseData.age}</h2>
+                <h2>Bio:${parseData.about}</h2>`);
+          }
+        );
+      }
+      else {
+    // else if (req.method === 'GET' && parsedUrl.pathname === '/allusers') {
         
-        fs.readdirSync(__dirname + '/contacts').filter((file) => {
+        res.setHeader('Content-Type', 'text/html');
+        fs.readdirSync(__dirname + '/contacts').forEach((file) => {
           fs.readFile(
             __dirname + '/contacts/' + file,
             'utf-8',
             (err, content) => {
               console.log(content);
               
-          
-          
+              var data = JSON.parse(content.toString());
+              var user = `<h1>Name:${data.name}</h1>
+              <h2>Email:${data.email}</h2>
+              <h2>Username:${data.username}</h2>
+              <h2>Age:${data.age}</h2>
+              <h2>Bio:${data.about}</h2><hr>`              
+             res.write(user);          
             });
             
         });
-        
-      }
+       setTimeout(()=>{res.end()},1000);//since  readFile is an async operation 
+     }
+    } 
+    else{
+       res.end('Page not Found');
+    } 
   });
 
 }
